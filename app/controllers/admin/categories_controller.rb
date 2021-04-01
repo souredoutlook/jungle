fa51ad2dcc22fb92d8cffbb003075dca2981ec8b
souredoutlook <souredoutlook@gmail.com>
2021-04-01 13:52:06 -0400

    add logic categories controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,36 @@
 class Admin::CategoriesController < ApplicationController
+
+  http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PASSWORD']
+
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Product created!'
+    else
+      render :new
+    end
+  end
+  
+  private
+
+  def product_params
+    params.require(:category).permit(
+      # :name,
+      # :description,
+      # :category_id,
+      # :quantity,
+      # :image,
+      # :price
+    )
+  end
+
 end
