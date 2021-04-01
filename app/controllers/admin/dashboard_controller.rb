c265dd7195cb6eeac8374f044f271c93600f8feb
souredoutlook <souredoutlook@gmail.com>
2021-03-31 23:01:47 -0400

    add http authentication to dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,11 @@
 class Admin::DashboardController < ApplicationController
+  # @admin_name = ENV['ADMIN_NAME'].to_s
+  # @admin_password = ENV['ADMIN_PASSWORD'].to_s
+
+  # http_basic_authenticate_with name: ENV['ADMIN_NAME'].to_s, password: ENV['ADMIN_PASSWORD'].to_s
+
+  http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PASSWORD']
+
   def show
   end
 end
