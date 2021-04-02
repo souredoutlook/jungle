a780dcfac9ea7b560b03a8d9944908633066c220
souredoutlook <souredoutlook@gmail.com>
2021-04-01 22:52:08 -0400

    add validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
-
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
+  
   has_secure_password
   
 end
