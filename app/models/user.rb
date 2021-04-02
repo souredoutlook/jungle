7efc236e1bed7b15c2d329dec17d76c222ff2145
souredoutlook <souredoutlook@gmail.com>
2021-04-02 00:46:08 -0400

    validate email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
+  validates :email, uniqueness: true
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
   
