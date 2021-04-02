e61fec2cdcaefe3ac3c3c41554ba07e8d83e32b1
souredoutlook <souredoutlook@gmail.com>
2021-04-01 20:50:40 -0400

    add login, logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   get 'about', to: 'about#index'
 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
   get '/signup', to: 'users#new'
   post '/users', to: 'users#create'
 
