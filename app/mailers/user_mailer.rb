fadbd66bba9863554b8ec74032683eb64dafff44
souredoutlook <souredoutlook@gmail.com>
2021-04-02 20:50:57 -0400

    add order_confirmation_email to UserMailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,8 @@
 class UserMailer < ApplicationMailer
+  default from: 'orders@jungle.com'
+ 
+  def order_confirmation_email(order)
+    @order = order
+    mail(to: order.email, subject: "Jungle Order ##{order.id}")
+  end
 end
