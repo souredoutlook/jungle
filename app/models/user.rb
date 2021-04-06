88fe4a3f7b7053817af342aac2248a2659e02c56
souredoutlook <souredoutlook@gmail.com>
2021-04-05 20:55:42 -0400

    add support for invalid email to authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
 
   def self.authenticate_with_credentials(email, password)
     user = User.find_by_email(email)
-    if user.authenticate(password)
+    if user && user.authenticate(password)
       user
     else
       nil
