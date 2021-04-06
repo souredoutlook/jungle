da4108a0741ae07c2f14e4d1028012aa8403b930
souredoutlook <souredoutlook@gmail.com>
2021-04-05 20:46:32 -0400

    method self.authenticate_with_credentials to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,14 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true
   
   has_secure_password
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email)
+    if user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
   
 end
