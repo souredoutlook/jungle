fdc07d7bf06ac90d01ec41e1e5f920784eb7f4a4
souredoutlook <souredoutlook@gmail.com>
2021-04-05 20:47:06 -0400

    refactor sessions controller to use new auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,7 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by_email(params[:email])
-    # If the user exists AND the password entered is correct.
-    if user && user.authenticate(params[:password])
+    if user = User.authenticate_with_credentials(params[:email], params[:password])
       # Save the user id inside the browser cookie. This is how we keep the user 
       # logged in when they navigate around our website.
       session[:user_id] = user.id
@@ -19,7 +17,7 @@ class SessionsController < ApplicationController
 
       redirect_to @prev_url
     else
-    # If user's login doesn't work, send them back to the login form.
+      # If user's login doesn't work, send them back to the login form.
       redirect_to '/login', alert: "Incorrect email or password"
     end
   end
