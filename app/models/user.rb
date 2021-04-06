371ba54c699e3eb09ba1c54cd25f096cca463d05
souredoutlook <souredoutlook@gmail.com>
2021-04-05 20:06:22 -0400

    add case insensitivity to uniqueness option

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
-  validates :email, uniqueness: true
+  validates :email, uniqueness: {case_sensitive: false}
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
   
