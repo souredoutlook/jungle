5ad7567abd7864cfa824d8fd7b3f6d9297055144
souredoutlook <souredoutlook@gmail.com>
2021-04-05 20:34:17 -0400

    add password length validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true
   validates :email, uniqueness: {case_sensitive: false}
   validates :password, confirmation: true
+  validates :password, length: {minimum: 8}
   validates :password_confirmation, presence: true
   
   has_secure_password
